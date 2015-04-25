72e4eedf3d7e6ec68f6aa9a67f66534eff09bb32
aaron <a.aa.akio.aaron@gmail.com>
2015-04-25 13:53:42 -0700

    multiple views

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,12 @@
 console.log('ready')
+
+setupNavigation('landing')
+setupNavigation('board-vapor')
+setupNavigation('board-new')
+
+function setupNavigation(name) {
+  $('.btn-'+name).on('click', function(){
+    $('.primary-content').removeClass('active-primary-content')
+    $('.content-'+name).addClass('active-primary-content')
+  })  
+}
